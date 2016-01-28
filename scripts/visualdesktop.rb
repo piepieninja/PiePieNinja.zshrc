641644d796af924109cb1022df733e9b6b2afddd
Caleb Adams <CalebAshmoreAdams@gmail.com>
2016-01-27 19:24:37 -0500

    daily commit

---
+++ scripts/visualdesktop.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+##
+# Author: Caleb Adams
+# Contact: CalebAshmoreAdams@gmail.com
+# Website: CalebDevelops.com 
+##
+
+require 'colorize'
+# ੦ᐅᖱ•‣∮ ⌘ ⏸ ⏹ ⏯ ⏲ ☞  ⚡ ✖  ♬ 
+
+cmd = "/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background"
+run = `#{cmd}`
+
+puts ">> RUNNING VISUAL DESKTOP <<"
+while true
+  puts run  
+end
+
